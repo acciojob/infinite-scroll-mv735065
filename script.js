492d6b3de019f1281df077b5135ca7da2177c6a4
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-10-30 19:09:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
+let box=document.querySelector("#infi-list");
+let c=0;
+function addItemsToBox(num) {
+for(let i=1;i<=num;i++){
+	c++;
+	let list=document.createElement("li");
+	list.innerHTML = `Item ${c}`;
+	box.append(list);
+}
+}
+
+addItemsToBox(10);
+function checkScroll() {
+	if (box.scrollTop + box.clientHeight >= box.scrollHeight - 10) {
+        addItemsToBox(2); // Add 2 more items
+    }
+	
+}
+
+
+
+box.addEventListener("scroll", checkScroll);
 
